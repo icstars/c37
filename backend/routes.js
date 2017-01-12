319e305af695ae1887a05bbad264380ad42e9e02
Dan LaFeir <danlafeir@gmail.com>
2017-01-11 23:41:39 -0500

    Dan | Storing images example

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,8 @@
 
 var router = require('express').Router();
 var database = require('./c37_modules/database');
+var multer  = require('multer');
+var upload = multer({ dest: 'frontend/images/' });
 
 module.exports = function() {
 
@@ -11,6 +13,10 @@ module.exports = function() {
 
   router.get('/trains', function(req,res){
     return res.render('trains.html');
+  });
+
+  router.get('/image', function(req, res){
+    return res.render('image_upload.html');
   });
 
   router.get('/trains/data', function(req,res){
@@ -24,6 +30,12 @@ module.exports = function() {
       res.send(results);
     });
   });
+
+  router.post('/image/upload', upload.single('image'), function (req, res){
+    var filename = req.file.filename
+    console.log('File was uploaded: ' + filename);
+    return res.send("<html><img src='/images/" + filename + "'/></html>")
+  }); 
 
   /* Your code here */
 
