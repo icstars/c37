a792637a0aace938a1bda6684f5effb4f656e9f3
dlaffy <dlaffy@users.noreply.github.com>
2017-04-17 12:50:15 -0500

    Dan | Reorganize for generic trains_example

--- app/backend/routes.js
+++ app/backend/routes.js
@@ -1,17 +1,13 @@
 'use strict'
 
 var router = require('express').Router();
-var database = require('./c37_modules/database');
+var database = require('./tools/database');
 var multer  = require('multer');
 var upload = multer({ dest: 'frontend/images/' });
 
 module.exports = function() {
 
   router.get('/', function(req,res){
-    return res.render('helloworld.html');
-  });
-
-  router.get('/trains', function(req,res){
     return res.render('trains.html');
   });
 
